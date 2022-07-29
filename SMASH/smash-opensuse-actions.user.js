8935ceddda371aacf4dea2d80adabfac7832c490
Gabriele Sonnu <gabriele.sonnu@suse.com>
2022-07-29 16:50:03 +0200

    smash-opensuse-actions.user.js: quick fix for SMASH bugs (always VUL-0, duplicated CVEs)

--- SMASH/smash-opensuse-actions.user.js
+++ SMASH/smash-opensuse-actions.user.js
@@ -4,7 +4,7 @@
 // @match       https://smash.suse.de/issue/*
 // @run-at      document-end
 // @grant       none
-// @version     1.2.2
+// @version     1.2.3
 // @author      gsonnu
 // @description Adds a button to open a bug in the openSUSE bugzilla tracker. Also updates the action item in the Actions dropdown with the same link
 // ==/UserScript==
@@ -13,6 +13,20 @@
     function updateElement(li, btn, proj) {
         btn.title = `${btn.title} (${proj})`;
         li.firstChild.nodeValue = `${proj} ${li.firstChild.nodeValue}`;
+    }
+
+    function updateLinks(btn, query, url) {
+        btn.href = url.href;
+
+        let link = document.querySelector(`.dropdown-item[href*="https://bugzilla"]${query}`);
+
+        if (!link)
+            return;
+
+        console.log(link.href)
+        console.log(url.href)
+
+        link.href = url.href;
     }
 
     let btn = document.querySelector('.btn.btn-sm[href*="https://bugzilla"]');
@@ -33,25 +47,27 @@
 
     // update url
     let url = new URL(btn_new.href);
+    let components = url.searchParams.get('short_desc').split(': ');
 
+    // always use VUL-0
+    components[0] = 'VUL-0';
+
+    // fix multiple CVES
+    components[1] = [...new Set(components[1].split(','))].join(',');
+
+    url.searchParams.set('short_desc', components.join(': '));
+
+    updateLinks(btn, '[title="Open bug"]', url);
+
+    // openSUSE
     url.searchParams.set('product', 'openSUSE Distribution');
     url.searchParams.set('component', 'Security');
     url.searchParams.set('version', 'Leap 15.4');
     url.searchParams.set('cc', '');
     url.searchParams.set('qa_contact', 'security-team@suse.de');
 
-    btn_new.href = url.href;
-
     li.parentNode.insertBefore(li_new, li.nextSibling);
 
-    // also edit the existing action item
-    let item = document.querySelector('.dropdown-item[href*="https://bugzilla"][title~="openSUSE"]');
+    updateLinks(btn_new, '[title~="openSUSE"]', url);
 
-    if (!item)
-        return;
-
-    console.log(item.href)
-    console.log(url.href)
-
-    item.href = url.href;
 })();
