da4fe481ab2b7673666bc687ac0e9c351fbecac8
Gabriele Sonnu <gabriele.sonnu@suse.com>
2023-10-10 14:58:12 +0200

    bugzilla-meta-suse-beautifier.user.js: new meta script

---
+++ Bugzilla/bugzilla-meta-suse-beautifier.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        Bugzilla - SUSE Beautifier
+// @namespace   SUSE SecTools
+// @match       https://bugzilla.suse.com/show_bug.cgi?*
+// @match       https://bugzilla.opensuse.org/show_bug.cgi?*
+// @run-at      document-end
+// @grant       none
+// @require     ./bugzilla-redirect-to-com.user.js
+// @require     ./bugzilla-fix-background.user.js
+// @require     ./bugzilla-bug-status-in-title.user.js
+// @require     ./bugzilla-add-security-info.user.js
+// @require     ./bugzilla-hide-bots-comments.user.js
+// @require     ./bugzilla-add-suse-cve-links.user.js
+// @version     0.0.1
+// @author      gsonnu
+// @description Meta userscript
+// ==/UserScript==
+
+(function (){
+    ;
+})();
